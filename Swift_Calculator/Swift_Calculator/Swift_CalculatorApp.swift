a7d0fd53f03b648b995e3127f8255d8ef6476306
Eva Fadhillah Ulia <115793590+evafadhillahulia@users.noreply.github.com>
2024-12-25 02:49:05 +0700

    Add files via upload

---
+++ Swift_Calculator/Swift_Calculator/Swift_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_CalculatorApp.swift
+//  Swift_Calculator
+//
+//  Created by Eva Fadhillah Ulia on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
